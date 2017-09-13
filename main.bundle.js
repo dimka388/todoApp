8fa03040765a216993504d766ee3e683b1ede619
Dmytro Rozhkov <dmytro.rozhkov@synapse.com>
2017-09-13 18:36:57 +0300

    update

--- main.bundle.js
+++ main.bundle.js
@@ -20,7 +20,7 @@ webpackEmptyAsyncContext.id = "../../../../../src/$$_gendir lazy recursive";
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"header\">\n\t<h1>Todos:</h1>\n</div>\n<ul class=\"todo-list\">\n\t<li *ngFor=\"let item of items\"\n\t\t[ngClass]=\"{'complete' : item.complete}\">\n\t\t\t<input type=\"checkbox\"\n\t\t\t\tvalue=\"done\"\n\t\t\t\t[checked]=\"item.complete\"\n\t\t\t\t(change)=\"onChangeStatus($event, item)\">\n\t\t\t<span class=\"text\">{{item.value}}</span>\n\t\t\t<span class=\"priority\"\n\t\t\t\t[ngClass]=\"item.priority\">{{item.priority}}</span>\n\t\t\t<button (click)=\"itemRemove(item)\">Delete</button>\n\t</li>\n</ul>\n<form class=\"todo-form\" novalidate #itemForm=\"ngForm\"\n\t(ngSubmit)=\"onSubmit(itemForm, $event)\">\n\t<input type=\"text\" name=\"text\" ngModel>\n\t<select name=\"priority\" ngModel>\n\t\t<option value=\"low\">low</option>\n\t\t<option value=\"medium\">medium</option>\n\t\t<option value=\"high\">high</option>\n\t</select>\n\t<button type=\"submit\">Add</button>\n</form>"
+module.exports = "<div class=\"wrapper\">\n\t<div class=\"page-header\">\n\t\t<h1>Todos:</h1>\n\t</div>\n\t<ul class=\"todo-list list-group\">\n\t\t<li *ngFor=\"let item of items\"\n\t\t\tclass=\"list-group-item\"\n\t\t\t[ngClass]=\"{'complete' : item.complete}\">\n\t\t\t\t<span class=\"text\">{{item.value}}</span>\n\t\t\t\t<select class=\"priority\"\n\t\t\t\t\tname=\"priority\"\n\t\t\t\t\t[ngClass]=\"item.priority\"\n\t\t\t\t\t(change)=\"itemPriorityChange($event, item)\">\n\t\t\t\t\t<option *ngFor='let priorityItem of priorityList'\n\t\t\t\t\t\t[attr.selected]=\"item.priority === priorityItem ? true : null\"\n\t\t\t\t\t\t[attr.value]='priorityItem'>\n\t\t\t\t\t\t{{priorityItem}}\n\t\t\t\t\t</option>\n\t\t\t\t</select>\n\t\t\t\t<div class=\"btn-group\" role=\"group\" aria-label=\"...\">\n\t\t\t\t\t<button  type=\"button\" class=\"btn btn-default\" (click)=\"itemUp(item)\">\n\t\t\t\t\t\t<span class=\"glyphicon glyphicon-chevron-up\" aria-hidden=\"true\"></span>\n\t\t\t\t\t</button>\n\t\t\t\t\t<button  type=\"button\" class=\"btn btn-default\" (click)=\"itemDown(item)\">\n\t\t\t\t\t\t<span class=\"glyphicon glyphicon-chevron-down\" aria-hidden=\"true\"></span>\n\t\t\t\t\t</button>\n\t\t\t\t\t<button  type=\"button\" class=\"btn btn-default\" [ngClass]=\"{'btn-primary' : item.complete}\" (click)=\"itemDone(item)\">\n\t\t\t\t\t\t<span class=\"glyphicon glyphicon-ok\" aria-hidden=\"true\"></span>\n\t\t\t\t\t</button>\n\t\t\t\t\t<button type=\"button\" class=\"btn btn-default\" (click)=\"itemRemove(item)\">\n\t\t\t\t\t\t<span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>\n\t\t\t\t\t</button>\n\t\t\t\t</div>\n\t\t</li>\n\t</ul>\n\n\t<form class=\"todo-form\" novalidate #itemForm=\"ngForm\"\n\t\t(ngSubmit)=\"onSubmit(itemForm, $event)\">\n\t\t<div class=\"input-group\">\n\t\t\t<input type=\"text\"\n\t\t\t\tngModel\n\t\t\t\tname=\"text\"\n\t\t\t\tclass=\"form-control\"\n\t\t\t\tautocomplete=\"off\">\n\t\t</div>\n\t\t<select name=\"priority\" ngModel>\n\t\t\t<option *ngFor='let priorityItem of priorityList'\n\t\t\t\t[attr.value]='priorityItem'>\n\t\t\t\t{{priorityItem}}\n\t\t\t</option>\n\t\t</select>\n\t\t<button type=\"submit\" class=\"btn btn-default\">\n\t\t\t<span class=\"glyphicon glyphicon-plus\" aria-hidden=\"true\"></span>\n\t\t</button>\n\t</form>\n</div>"
 
 /***/ }),
 
@@ -32,7 +32,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, ".header {\n  text-align: center;\n  padding: 20px 0; }\n\n.todo-list {\n  margin: 0 auto 30px;\n  max-width: 600px; }\n  .todo-list li {\n    display: -webkit-box;\n    display: -ms-flexbox;\n    display: flex;\n    margin: 0 0 20px;\n    line-height: 20px;\n    -webkit-box-pack: justify;\n        -ms-flex-pack: justify;\n            justify-content: space-between; }\n    .todo-list li.complete .text {\n      text-decoration: line-through; }\n  .todo-list .text {\n    width: calc(100% - 220px); }\n  .todo-list .priority {\n    min-width: 70px;\n    text-align: center;\n    border-radius: 5px;\n    border: 1px solid #333;\n    background: white; }\n    .todo-list .priority.low {\n      background: rgba(0, 128, 0, 0.3); }\n    .todo-list .priority.medium {\n      background: rgba(255, 255, 0, 0.3); }\n    .todo-list .priority.high {\n      background: rgba(255, 0, 0, 0.3); }\n  .todo-list button {\n    min-width: 70px;\n    border-radius: 5px;\n    border: 1px solid #333;\n    text-align: center;\n    cursor: pointer; }\n\n.todo-form {\n  margin: 0 auto;\n  max-width: 300px;\n  display: -webkit-box;\n  display: -ms-flexbox;\n  display: flex;\n  -webkit-box-pack: justify;\n      -ms-flex-pack: justify;\n          justify-content: space-between; }\n", ""]);
+exports.push([module.i, ".wrapper {\n  max-width: 600px;\n  margin: 0 auto; }\n\n.page-header {\n  text-align: center;\n  margin: 0 0 40px;\n  padding: 40px 0; }\n  .page-header h1 {\n    margin: 0; }\n\n.todo-list {\n  margin: 0 auto 30px;\n  max-width: 600px;\n  counter-reset: section;\n  list-style-type: none; }\n  .todo-list li {\n    display: -webkit-box;\n    display: -ms-flexbox;\n    display: flex;\n    -webkit-box-pack: justify;\n        -ms-flex-pack: justify;\n            justify-content: space-between; }\n    .todo-list li:before {\n      counter-increment: section;\n      content: counters(section, \".\") \". \"; }\n    .todo-list li.complete .text {\n      text-decoration: line-through; }\n  .todo-list .text {\n    width: calc(100% - 270px); }\n  .todo-list .priority {\n    min-width: 70px;\n    text-align: center;\n    border-radius: 5px;\n    border: 1px solid #333;\n    background: white; }\n    .todo-list .priority.low {\n      background: rgba(0, 128, 0, 0.3); }\n    .todo-list .priority.medium {\n      background: rgba(255, 255, 0, 0.3); }\n    .todo-list .priority.high {\n      background: rgba(255, 0, 0, 0.3); }\n\n.todo-form {\n  display: -webkit-box;\n  display: -ms-flexbox;\n  display: flex;\n  -webkit-box-pack: justify;\n      -ms-flex-pack: justify;\n          justify-content: space-between; }\n  .todo-form .input-group {\n    width: calc(100% - 150px); }\n  .todo-form select {\n    text-align: center;\n    min-width: 70px;\n    border-radius: 5px;\n    border: 1px solid #333;\n    background: white; }\n", ""]);
 
 // exports
 
@@ -69,10 +69,11 @@ var AppComponent = (function () {
         var _this = this;
         this.http = http;
         this.src = 'https://59b68e8ed7d6800011e572f5.mockapi.io/items';
+        this.priorityList = ['low', 'medium', 'high'];
         this.items = [];
         this.getItems().subscribe(function (data) {
             data.sort(function (a, b) {
-                return +a.index > +b.index;
+                return a.index - b.index;
             });
             _this.items = data;
         });
@@ -86,14 +87,67 @@ var AppComponent = (function () {
         this.http.delete(this.src + '/' + obj.id)
             .subscribe(function () { return console.warn('Item removed:' + obj.value); });
     };
-    AppComponent.prototype.onChangeStatus = function (e, obj) {
+    AppComponent.prototype.itemDone = function (obj) {
         var _this = this;
         this.items.map(function (item) {
             if (item.id === obj.id) {
-                item.complete = e.target.checked;
+                item.complete = !item.complete;
                 _this.http.put(_this.src + '/' + item.id, item)
                     .subscribe(function () { return console.warn('Item updated:' + item.value); });
             }
+        });
+    };
+    AppComponent.prototype.itemPriorityChange = function (e, obj) {
+        var _this = this;
+        this.items.map(function (item) {
+            if (item.id === obj.id) {
+                item.priority = e.target.value;
+                _this.http.put(_this.src + '/' + item.id, item)
+                    .subscribe(function () { return console.warn('Item updated:' + item.value); });
+            }
+        });
+    };
+    AppComponent.prototype.itemOrderChange = function (currentItem, otherItems, decrement) {
+        var newIndex = null;
+        var updatedItems = [];
+        var getIndex = function (index) {
+            if (decrement) {
+                return newIndex === null || index >= newIndex;
+            }
+            else {
+                return newIndex === null || newIndex > index;
+            }
+        };
+        if (otherItems.length) {
+            if (decrement) {
+                otherItems.sort(function (a, b) { return b.index - a.index; });
+            }
+            otherItems.map(function (item) {
+                if (getIndex(item.index)) {
+                    newIndex = item.index;
+                    item.index = currentItem.index;
+                    updatedItems.push(item);
+                }
+            });
+            currentItem.index = newIndex;
+            updatedItems.push(currentItem);
+            this.onChangeOrder(updatedItems);
+        }
+    };
+    AppComponent.prototype.itemUp = function (currentItem) {
+        this.itemOrderChange(currentItem, this.items.filter(function (item) { return item.index < currentItem.index; }), true);
+    };
+    AppComponent.prototype.itemDown = function (currentItem) {
+        this.itemOrderChange(currentItem, this.items.filter(function (item) { return item.index > currentItem.index; }), false);
+    };
+    AppComponent.prototype.onChangeOrder = function (items) {
+        var _this = this;
+        items.map(function (item) {
+            _this.http.put(_this.src + '/' + item.id, item)
+                .subscribe(function () { return console.warn('Item updated:' + item.value); });
+        });
+        this.items.sort(function (a, b) {
+            return a.index - b.index;
         });
     };
     AppComponent.prototype.onSubmit = function (itemForm, event) {
@@ -101,9 +155,12 @@ var AppComponent = (function () {
         var newItem = {
             id: this.items.length + 1,
             value: itemForm.value.text,
-            priority: itemForm.value.priority || 'normal',
+            priority: itemForm.value.priority || this.priorityList[0],
             index: this.items.length + 1
         };
+        if (!itemForm.value.text.length) {
+            return;
+        }
         this.items.push(newItem);
         this.http.post(this.src, newItem)
             .subscribe(function () { return console.warn('Item added:' + newItem.value); });
